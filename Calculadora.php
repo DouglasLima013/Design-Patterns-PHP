d6f7b61fbabb979a2668d2b9d5d3b772ff68c074
Douglas O. Lima <douglaslima1395@gmail.com>
2017-11-18 15:43:14 -0200

    Eliminando IF's

--- Calculadora.php
+++ Calculadora.php
@@ -1,20 +1,8 @@
 <?php
-	require 'ICMS.php';
-	require 'ISS.php';
-
 	class Calculadora
 	{
 		function calcula(orcamento $orcamento, $imposto)
 		{
-			if ($imposto == "ICMS")
-			{
-				$icms = new ICMS;
-				return $icms->calcula($orcamento);
-			}
-			elseif ($imposto == "ISS")
-			{
-				$iss = new ISS;
-				return $iss->calcula($orcamento);
-			}
+			return $imposto->calcula($orcamento);
 		}
 	}
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 	require 'Orcamento.php';
 	require 'Calculadora.php';
+	require 'ICMS.php';
+	require 'ISS.php';
 
 	$reforma = new Orcamento(500);
 	$calculadora = new Calculadora;
 
-	echo $calculadora->calcula($reforma, "ICMS");
+	echo $calculadora->calcula($reforma, new ICMS);
 
 	echo "<br>";
 
-	echo $calculadora->calcula($reforma, "ISS");
+	echo $calculadora->calcula($reforma, new ISS);
